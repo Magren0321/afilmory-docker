6e3209462dd725e8da7039d00f6608193fcc9866
Magren <zhuhenglin21@gmail.com>
2025-10-30 01:39:49 +0800

    chore: clean up builder.config.ts by removing commented lines and standardizing quotes

--- builder.config.ts
+++ builder.config.ts
@@ -2,13 +2,11 @@ import { defineBuilderConfig } from '@afilmory/builder'
 
 export default defineBuilderConfig(() => ({
   repo: {
-    // Use remote repository as manifest and thumbnail cache
     enable: false,
     url: 'https://github.com/username/gallery-public',
   },
   storage: {
-    // Storage configuration
-    provider: 's3',
+    provider: "s3",
     bucket: 'gallery',
     region: 'us-east-1',
     prefix: '/',
