69e0d2d4540e6f5b28dafbeaf4e79db3258ad08a
Magren <zhuhenglin21@gmail.com>
2025-10-31 02:24:24 +0800

    chore: update S3 prefix configuration in builder settings

--- builder.config.ts
+++ builder.config.ts
@@ -10,7 +10,7 @@ export default defineBuilderConfig(() => ({
     provider: "s3",
     bucket: 'gallery',
     region: 'us-east-1',
-    prefix: '/',
+    prefix: '',
     customDomain: 'https://gallery.magren.cc',
     endpoint: "https://fd2117ae96666e795bf2bce76289651b.r2.cloudflarestorage.com",
     accessKeyId: env.S3_ACCESS_KEY_ID,
