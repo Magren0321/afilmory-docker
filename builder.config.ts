b25e3cb89c9c782cc1d99d4f31638ad438f92a20
Magren <zhuhenglin21@gmail.com>
2025-10-31 01:58:22 +0800

    chore: add S3 endpoint and storage plugin to builder configuration

--- builder.config.ts
+++ builder.config.ts
@@ -11,5 +11,9 @@ export default defineBuilderConfig(() => ({
     region: 'us-east-1',
     prefix: '/',
     customDomain: 'https://gallery.magren.cc',
+    endpoint: "https://fd2117ae96666e795bf2bce76289651b.r2.cloudflarestorage.com"
   },
+  plugins: [
+    '@afilmory/builder/plugins/storage/s3.js'
+  ]
 }))
