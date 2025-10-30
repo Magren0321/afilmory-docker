9964a7ac733fd20b4f2be865eb872c89ef2c11ee
Magren <zhuhenglin21@gmail.com>
2025-10-31 02:17:10 +0800

    chore: add access key configuration for S3 storage in builder settings

--- builder.config.ts
+++ builder.config.ts
@@ -11,7 +11,9 @@ export default defineBuilderConfig(() => ({
     region: 'us-east-1',
     prefix: '/',
     customDomain: 'https://gallery.magren.cc',
-    endpoint: "https://fd2117ae96666e795bf2bce76289651b.r2.cloudflarestorage.com"
+    endpoint: "https://fd2117ae96666e795bf2bce76289651b.r2.cloudflarestorage.com",
+    accessKeyId: env.S3_ACCESS_KEY_ID,
+    secretAccessKey: env.S3_SECRET_ACCESS_KEY,
   },
   plugins: [
     '@afilmory/builder/plugins/storage/s3.js'
